a7e89c436eaba23067a1cf560a43d0755f29193b
Vansh <nexusop82@gmail.com>
2026-09-14 21:37:59 +0530

    chore: Remove test script

--- test.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-prisma.task.findMany({ select: { id: true, ticketId: true, title: true } }).then(console.log).finally(() => prisma.$disconnect());
